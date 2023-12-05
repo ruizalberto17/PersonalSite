3b4604091a1e3284ef76f6bf8cf8172682ac7a7a
Alberto Ruiz <ruizalberto17@gmail.com>
2023-12-05 15:31:50 -0800

    Added services to the startup.

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PersonalSite.Services;
 
 namespace PersonalSite
 {
@@ -19,6 +20,8 @@ namespace PersonalSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddMvc();
+            services.AddTransient<IMailService, SmtpMailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
